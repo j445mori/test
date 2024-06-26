8705958c53414f2739365fa402ce9295fdfcd64e
j445mori <j520445z@mails.cc.ehime-u.ac.jp>
2024-06-26 10:36:39 +0900

    Update a.php
    $lineにxyが含まれていたらその行を表示するようにした。

--- a.php
+++ a.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 foreach(file('/usr/share/dict/words') as $line){
-    if(strpos($line, "abc") !== false) {
+    if(strpos($line, "xy") !== false) {
         echo "$line";
     }
 }
