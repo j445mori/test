358614ef6db2a95e3413a63f8eaad937e5d89175
j445mori <j520445z@mails.cc.ehime-u.ac.jp>
2024-06-26 10:58:30 +0900

    Update main.c
    x<=0を!(x>=0)に変更してエラー処理ができるようにした。

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないときエラー表示を行い exit(1) とする。 
 {
   int y;
-  if( x <= 0 ){
+  if(!( x >= 0 )){
     printf("エラー：0以上の値ではありません in myRoot()\n");　      
     exit(1);
   }
